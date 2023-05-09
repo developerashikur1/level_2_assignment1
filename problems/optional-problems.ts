ea5a1c7da665e5fa0a8fe34bf9656de831af4782
developerashikur1 <masik4506@gmail.com>
2023-05-09 23:29:30 +0600

    qustion solved

---
+++ problems/optional-problems.ts
@@ -0,0 +1,13 @@
+// optional Problem 1
+// Array and Tuple types
+
+// Create a TypeScript program that declares an array of strings and a tuple of a number and a string. Add elements to both data structures, and log them to the console.
+
+const arrOfStrings = ["sabbir", "kabbir", "nabbir"];
+
+// Optional Problem 2:
+// Create a TypeScript program that uses a conditional type to create a new type that is either a string or a number, depending on whether a parameter of type string or number is passed to the function.
+
+// Then, create an interface for a person with properties name and age, and use the new type as a constraint on the age property.
+
+// Finally, create an object that satisfies the Person interface and log it to the console.
