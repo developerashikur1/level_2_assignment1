a41f1710b414c98b4b4449c40cfed5d134ae6032
developerashikur1 <masik4506@gmail.com>
2023-05-09 23:37:42 +0600

    git push

--- problems/optional-problems.ts
+++ problems/optional-problems.ts
@@ -3,7 +3,14 @@
 
 // Create a TypeScript program that declares an array of strings and a tuple of a number and a string. Add elements to both data structures, and log them to the console.
 
-const arrOfStrings = ["sabbir", "kabbir", "nabbir"];
+const arrOfStrings: string[] = ["sabbir", "kabbir", "nabbir"];
+const tupleOfNUm: [number, string] = [12, "Shonibar"];
+
+arrOfStrings.push("Ashikur");
+arrOfStrings.push("Shadhin");
+
+tupleOfNUm[0] = 16;
+tupleOfNUm[1] = "ashikur";
 
 // Optional Problem 2:
 // Create a TypeScript program that uses a conditional type to create a new type that is either a string or a number, depending on whether a parameter of type string or number is passed to the function.
